96c8a7a3846ec3128876a59c7e91b9a700a10cbd
Rita <ritver@gmail.com>
2019-07-21 19:03:29 +0300

    Atlikta 7.2. užduotis

---
+++ namudarbai/7.2. užduotis/comments.php
@@ -0,0 +1,9 @@
+<?php
+echo"Vilnius<br>"; // Vilnius is the capital of Lithuania.
+echo"Riga<br>"; # Riga is the capital and primate city of Latvia. 
+echo"Tallin<br>";
+/*
+Tallinn is the capital, primate and the most 
+populous city of Estonia. 
+*/
+?>

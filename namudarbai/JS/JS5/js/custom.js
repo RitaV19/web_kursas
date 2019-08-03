16ccb213adf8f09d1b00061c075d932b66932fb9
Rita <ritver@gmail.com>
2019-08-03 22:19:51 +0300

    JS5 atlikta

--- namudarbai/JS/JS5/js/custom.js
+++ namudarbai/JS/JS5/js/custom.js
@@ -1,15 +1,10 @@
 function JeiguAmziusYra18IrDaugiau(amzius){
 	if(amzius >= 18){
 		return true;
-
-
-//		document.write("Jūs galite užeiti į svetainę!");
-
 	}
 
 	else {
 		return false;
-
 	}
 }
 

11d04d6c2fd2d5b625d93b42abb015236ef0fd76
Rita <ritver@gmail.com>
2019-08-03 19:30:06 +0300

    JS4 atlikta

---
+++ namudarbai/JS/JS4/js/custom.js
@@ -0,0 +1,11 @@
+function amzius18irDaugiau(amzius){
+	if(amzius >= 18){
+		document.write("Jūs galite užeiti į svetainę!");
+
+	}
+
+	else {
+		document.write("Į svetainę užeiti negalite!");
+	}
+}
+

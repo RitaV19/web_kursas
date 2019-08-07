7277b6ef9aab4e7d2f528749deae47b165c02768
Rita <ritver@gmail.com>
2019-08-07 23:19:57 +0300

    JS8 atlikta

---
+++ namudarbai/JS/JS8/js/custom.js
@@ -0,0 +1,13 @@
+function duokManSavaitesDienaPagalSkaiciu(index){
+	let savaitesDienos = [
+		"Sekmadienis",
+		"Pirmadienis",
+		"Antradienis",
+		"Treciadienis",
+		"Ketvirtadienis",
+		"Penktadienis",
+		"Sestadienis",
+	];
+
+		document.write(savaitesDienos[5])
+	}

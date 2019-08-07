34305d60edf94a53e2d4fda3a27aa5c624dbd12f
Rita <ritver@gmail.com>
2019-08-07 23:28:15 +0300

    JS9 atlikta

---
+++ namudarbai/JS/JS9/js/custom.js
@@ -0,0 +1,10 @@
+function spausdintiVardusIrPavardesISarasa(vardai){
+
+	document.write("<ol>");
+
+	for(let i = 0; i < vardai.length; i++){
+		document.write("<li>"+vardai[i]+"</li>");
+	}
+
+	document.write("</ol>");
+}
